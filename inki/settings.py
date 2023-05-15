db86b8acb7889b9eb0a8bd0b6d42b5b51497c8b3
tbro2020 <christian@tabaro.me>
2023-05-15 11:57:00 +0100

    deploy with inki

--- inki/settings.py
+++ inki/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 from django.contrib import messages
 
@@ -79,13 +80,10 @@ WSGI_APPLICATION = 'inki.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
+DATABASES = {'default': dj_database_url.config(
+    conn_max_age=600,
+    conn_health_checks=True,
+)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
